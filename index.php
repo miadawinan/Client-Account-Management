d255817ea6472b303aaf5b5be16c477229d55d05
miadawinan <miahdawinan@yahoo.com>
2016-02-27 00:04:25 +1300

    Create index.php

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>Untitled Document</title>
+</head>
+
+<body>
+<table width="800" height="395" border="0">
+  <tr>
+    <td colspan="3"><img src="NZCarelogo.png" alt="NzCare logo" width="95" height="125" /></td>
+  </tr>
+  <tr>
+    <td colspan="3">&nbsp;</td>
+  </tr>
+  <tr>
+    <td height="20" colspan="3" class="bar">&nbsp;</td>
+  </tr>
+  <tr>
+    <td width="250"><h3><span class="homepageText">Home page</span></h3></td>
+    <td width="433">&nbsp;</td>
+    <td width="103"><h4><a href="login.php" class="loginFont">Login</a></h4></td>
+  </tr>
+  <tr>
+    <td height="69" colspan="3"><h3><a href="index.php"></a></h3>
+      <p><a href="index.php" class="nzcareText"><strong>NZCare</strong></a></p>
+      <p>&nbsp;</p>
+      <p>&nbsp;</p></td>
+  </tr>
+  <tr>
+    <td colspan="3">&nbsp;</td>
+  </tr>
+</table>
+</body>
+</html>
