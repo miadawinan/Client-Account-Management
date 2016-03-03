a0fb6255a0c3004893549b24b69d2a644be77a70
miadawinan <miahdawinan@yahoo.com>
2016-03-03 22:42:32 +1300

    Added files via upload

---
+++ adminPage.php
@@ -0,0 +1,100 @@
+<?php @session_start(); ?>
+<?php require_once('Connections/MAMP.php'); ?>
+<?php
+if (!function_exists("GetSQLValueString")) {
+function GetSQLValueString($theValue, $theType, $theDefinedValue = "", $theNotDefinedValue = "") 
+{
+  if (PHP_VERSION < 6) {
+    $theValue = get_magic_quotes_gpc() ? stripslashes($theValue) : $theValue;
+  }
+
+  $theValue = function_exists("mysql_real_escape_string") ? mysql_real_escape_string($theValue) : mysql_escape_string($theValue);
+
+  switch ($theType) {
+    case "text":
+      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+      break;    
+    case "long":
+    case "int":
+      $theValue = ($theValue != "") ? intval($theValue) : "NULL";
+      break;
+    case "double":
+      $theValue = ($theValue != "") ? doubleval($theValue) : "NULL";
+      break;
+    case "date":
+      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+      break;
+    case "defined":
+      $theValue = ($theValue != "") ? $theDefinedValue : $theNotDefinedValue;
+      break;
+  }
+  return $theValue;
+}
+}
+
+$colname_user = "-1";
+if (isset($_SESSION['MM_Username'])) {
+  $colname_user = $_SESSION['MM_Username'];
+}
+mysql_select_db($database_MAMP, $MAMP);
+$query_user = sprintf("SELECT * FROM Users WHERE userName = %s", GetSQLValueString($colname_user, "text"));
+$user = mysql_query($query_user, $MAMP) or die(mysql_error());
+$row_user = mysql_fetch_assoc($user);
+$totalRows_user = mysql_num_rows($user);
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<link href="CSS/layout.css" rel="stylesheet" type="text/css" />
+<link href="CSS/menu.css" rel="stylesheet" type="text/css" />
+<style type="text/css">
+h1 {
+	font-weight: lighter;
+}
+</style>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>Admin</title>
+</head>
+
+<body>
+<div id="holder">
+<div id="header"></div>
+<div id="navBar">
+	<nav>
+    	<ul>
+        	<li><a href="account.php">My Profile</a>        	</li>
+        	<li><a href="adminClientPage.php">Clients</a></li>
+            <li><a href="#">Service Areas</a></li>
+            <li>Audit</li>
+            <li><a href="#">Employees</a></li>
+            <li></li>
+            <li><a href="logout.php">Logout</a></li>
+        </ul>
+    </nav>
+  </div>
+<div id="content">
+	<div id="pageHeading">
+	  <h1>Welcome, <?php echo $row_user['firstName']; ?> <?php echo $row_user['lastName']; ?>!</h1>
+	</div>
+    <div id="contentLeft">
+      <p>Account Links</p>
+      <p>&nbsp;</p>
+      <p>&nbsp;</p>
+    </div>
+    <div id="contentRight">
+      <form id="updateForm" name="updateForm" method="post" action="">
+        <table width="400" border="0" align="center">
+          <tr>          </tr>
+        <tr>          </tr>
+      </table>
+        <p>&nbsp;</p>
+      </form>
+    </div>
+  </div>
+<div id="footer"></div>
+</div>
+</body>
+</html>
+<?php
+mysql_free_result($user);
+?>
