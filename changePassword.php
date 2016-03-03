2f0a07531ef56d53463d2926a51fa18a13f88f36
miadawinan <miahdawinan@yahoo.com>
2016-03-03 22:44:06 +1300

    Added files via upload

---
+++ changePassword.php
@@ -0,0 +1,163 @@
+<?php @session_start(); ?>
+<?php require_once('Connections/MAMP.php'); ?>
+<?php
+if (!function_exists("GetSQLValueString")) {
+function GetSQLValueString($theValue, $theType, $theDefinedValue = "", $theNotDefinedValue = "") 
+{
+  if (PHP_VERSION < 6) {
+    $theValue = get_magic_quotes_gpc() ? stripslashes($theValue) : $theValue;
+  }
+
+  $theValue = function_exists("mysql_real_escape_string") ? mysql_real_escape_string($theValue) : mysql_escape_string($theValue);
+
+  switch ($theType) {
+    case "text":
+      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+      break;    
+    case "long":
+    case "int":
+      $theValue = ($theValue != "") ? intval($theValue) : "NULL";
+      break;
+    case "double":
+      $theValue = ($theValue != "") ? doubleval($theValue) : "NULL";
+      break;
+    case "date":
+      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+      break;
+    case "defined":
+      $theValue = ($theValue != "") ? $theDefinedValue : $theNotDefinedValue;
+      break;
+  }
+  return $theValue;
+}
+}
+
+$editFormAction = $_SERVER['PHP_SELF'];
+if (isset($_SERVER['QUERY_STRING'])) {
+  $editFormAction .= "?" . htmlentities($_SERVER['QUERY_STRING']);
+}
+
+if ((isset($_POST["MM_update"])) && ($_POST["MM_update"] == "updateForm")) {
+  $updateSQL = sprintf("UPDATE Users SET email=%s, password=%s WHERE userID=%s",
+                       GetSQLValueString($_POST['email'], "text"),
+                       GetSQLValueString($_POST['password'], "text"),
+                       GetSQLValueString($_POST['userID'], "int"));
+
+  mysql_select_db($database_MAMP, $MAMP);
+  $Result1 = mysql_query($updateSQL, $MAMP) or die(mysql_error());
+
+  $updateGoTo = "account.php";
+  if (isset($_SERVER['QUERY_STRING'])) {
+    $updateGoTo .= (strpos($updateGoTo, '?')) ? "&" : "?";
+    $updateGoTo .= $_SERVER['QUERY_STRING'];
+  }
+  header(sprintf("Location: %s", $updateGoTo));
+}
+
+$colname_user = "-1";
+if (isset($_SESSION['MM_Username'])) {
+  $colname_user = $_SESSION['MM_Username'];
+}
+mysql_select_db($database_MAMP, $MAMP);
+$query_user = sprintf("SELECT * FROM Users WHERE userName = %s", GetSQLValueString($colname_user, "text"));
+$user = mysql_query($query_user, $MAMP) or die(mysql_error());
+$row_user = mysql_fetch_assoc($user);
+$totalRows_user = mysql_num_rows($user);
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<link href="CSS/layout.css" rel="stylesheet" type="text/css" />
+<link href="CSS/menu.css" rel="stylesheet" type="text/css" />
+<style type="text/css">
+h1 {
+	font-weight: lighter;
+}
+</style>
+<link href="SpryAssets/SpryValidationTextField.css" rel="stylesheet" type="text/css" />
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>ChangePassword</title>
+<script src="SpryAssets/SpryValidationTextField.js" type="text/javascript"></script>
+</head>
+
+<body>
+<div id="holder">
+<div id="header"></div>
+<div id="navBar">
+	<nav>
+    	<ul>
+        	<li><a href="account.php">My Profile</a>        	</li>
+        	<li><a href="logout.php">Logout</a></li>
+            <li></li>
+        </ul>
+    </nav>
+    </div>
+<div id="content">
+	<div id="pageHeading">
+	  <h1>Change Password</h1>
+	</div>
+    <div id="contentLeft">
+      <h3>Account Links </h3>
+      <p><a href="updateProfile.php">Update Profile</a></p>
+      <p><a href="bankDetails.php">Bank Details</a></p>
+      <p><a href="wallet.php">Wallet Transactions</a></p>
+      <p><a href="changeClientPassword.php">Change Password</a></p>
+    </div>
+    <div id="contentRight">
+      <form id="updateForm" name="updateForm" method="POST" action="<?php echo $editFormAction; ?>">
+        <table width="600" border="0" align="center">
+          <tr>
+            <td>Account <?php echo $row_user['firstName']; ?> <?php echo $row_user['lastName']; ?>| Username: <?php echo $row_user['userName']; ?></td>
+          </tr>
+        </table>
+        <table width="400" border="0" align="center">
+          <tr>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+          </tr>
+          <tr>
+            <td width="83">Email:</td>
+            <td width="307"><span id="sprytextfield1">
+              <input name="email" type="text" id="email" value="<?php echo $row_user['email']; ?>" />
+            <span class="textfieldRequiredMsg">A value is required.</span></span></td>
+          </tr>
+          <tr>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+          </tr>
+          <tr>
+            <td>Password:</td>
+            <td><span id="sprytextfield2">
+              <label for="password"></label>
+              <input name="password" type="password" id="password" value="<?php echo $row_user['password']; ?>" />
+            <span class="textfieldRequiredMsg">A value is required.</span></span></td>
+          </tr>
+          <tr>
+            <td><input name="userID" type="hidden" id="userID" value="<?php echo $row_user['userID']; ?>" /></td>
+            <td>&nbsp;</td>
+          </tr>
+          <tr>
+            <td>&nbsp;</td>
+            <td><input type="submit" name="updateButton" id="updateButton" value="Update" /></td>
+          </tr>
+          <tr>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+          </tr>
+        </table>
+        <p>&nbsp;</p>
+        <input type="hidden" name="MM_update" value="updateForm" />
+      </form>
+    </div>
+  </div>
+<div id="footer"></div>
+</div>
+<script type="text/javascript">
+var sprytextfield1 = new Spry.Widget.ValidationTextField("sprytextfield1");
+var sprytextfield2 = new Spry.Widget.ValidationTextField("sprytextfield2");
+</script>
+</body>
+</html>
+<?php
+mysql_free_result($user);
+?>
