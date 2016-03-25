29273dbaf53a9e7dc7c6736c0dcb0fbd6c22c92d
miadawinan <miahdawinan@yahoo.com>
2016-03-25 21:58:35 +1300

    Update and rename adminAddClient.php to adminRegisterClient.php

--- adminRegisterClient.php
+++ adminRegisterClient.php
@@ -1,4 +1,3 @@
-<?php @session_start(); ?>
 <?php require_once('Connections/MAMP.php'); ?>
 <?php
 if (!isset($_SESSION)) {
@@ -33,7 +32,7 @@ function isAuthorized($strUsers, $strGroups, $UserName, $UserGroup) {
   return $isValid; 
 }
 
-$MM_restrictGoTo = "account.php";
+$MM_restrictGoTo = "staffAccess.php";
 if (!((isset($_SESSION['MM_Username'])) && (isAuthorized("",$MM_authorizedUsers, $_SESSION['MM_Username'], $_SESSION['MM_UserGroup'])))) {   
   $MM_qsChar = "?";
   $MM_referrer = $_SERVER['PHP_SELF'];
@@ -80,9 +79,9 @@ function GetSQLValueString($theValue, $theType, $theDefinedValue = "", $theNotDe
 // *** Redirect if username exists
 $MM_flag="MM_insert";
 if (isset($_POST[$MM_flag])) {
-  $MM_dupKeyRedirect="adminAddClient.php";
+  $MM_dupKeyRedirect="adminRegisterClient.php";
   $loginUsername = $_POST['username'];
-  $LoginRS__query = sprintf("SELECT Username FROM Clients WHERE Username=%s", GetSQLValueString($loginUsername, "text"));
+  $LoginRS__query = sprintf("SELECT userName FROM Users WHERE userName=%s", GetSQLValueString($loginUsername, "text"));
   mysql_select_db($database_MAMP, $MAMP);
   $LoginRS=mysql_query($LoginRS__query, $MAMP) or die(mysql_error());
   $loginFoundUser = mysql_num_rows($LoginRS);
@@ -104,22 +103,20 @@ if (isset($_SERVER['QUERY_STRING'])) {
 }
 
 if ((isset($_POST["MM_insert"])) && ($_POST["MM_insert"] == "addClientForm")) {
-  $insertSQL = sprintf("INSERT INTO Clients (Username, NHINumber, FirstName, LastName, Gender, ServiceID, PhoneNumber, EmergencyContactName, EmergencyContactAdd, EmergencyContactNo) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",
+  $insertSQL = sprintf("INSERT INTO Users (userName, firstName, lastName, contactNumber, serviceID, email, password, `role`) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)",
                        GetSQLValueString($_POST['username'], "text"),
-                       GetSQLValueString($_POST['nhiNumber'], "text"),
                        GetSQLValueString($_POST['firstName'], "text"),
                        GetSQLValueString($_POST['lastName'], "text"),
-                       GetSQLValueString($_POST['genderOption'], "text"),
-                       GetSQLValueString($_POST['serviceMenu'], "int"),
                        GetSQLValueString($_POST['contactNumber'], "int"),
-                       GetSQLValueString($_POST['emergencyContactName'], "text"),
-                       GetSQLValueString($_POST['emgncyContactAdd'], "text"),
-                       GetSQLValueString($_POST['emergencyContactNumber'], "int"));
+                       GetSQLValueString($_POST['serviceMenu'], "int"),
+                       GetSQLValueString($_POST['email'], "text"),
+                       GetSQLValueString($_POST['password'], "text"),
+                       GetSQLValueString($_POST['clientRole'], "text"));
 
   mysql_select_db($database_MAMP, $MAMP);
   $Result1 = mysql_query($insertSQL, $MAMP) or die(mysql_error());
 
-  $insertGoTo = "adminAddClient.php";
+  $insertGoTo = "adminClientRegSuccess.php";
   if (isset($_SERVER['QUERY_STRING'])) {
     $insertGoTo .= (strpos($insertGoTo, '?')) ? "&" : "?";
     $insertGoTo .= $_SERVER['QUERY_STRING'];
@@ -128,20 +125,26 @@ if ((isset($_POST["MM_insert"])) && ($_POST["MM_insert"] == "addClientForm")) {
 }
 
 if ((isset($_POST["MM_insert"])) && ($_POST["MM_insert"] == "addClientForm")) {
-  $insertSQL = sprintf("INSERT INTO Users (firstName, lastName, contactNumber, serviceAddress, email, userName, password, `role`) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)",
+  $insertSQL = sprintf("INSERT INTO Clients (Username, NHINumber, FirstName, LastName, Gender, AssignedSW, ServiceID, ServiceArea, PhoneNumber, Email, EmergencyContactName, EmergencyContactAdd, EmergencyContactNo, FinAgreement) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",
+                       GetSQLValueString($_POST['username'], "text"),
+                       GetSQLValueString($_POST['nhiNumber'], "text"),
                        GetSQLValueString($_POST['firstName'], "text"),
                        GetSQLValueString($_POST['lastName'], "text"),
+                       GetSQLValueString($_POST['genderOption'], "text"),
+                       GetSQLValueString($_POST['assignSW'], "text"),
+                       GetSQLValueString($_POST['serviceMenu'], "int"),
+                       GetSQLValueString($_POST['areaMenu'], "text"),
                        GetSQLValueString($_POST['contactNumber'], "int"),
-                       GetSQLValueString($_POST['serviceAddress'], "text"),
                        GetSQLValueString($_POST['email'], "text"),
-                       GetSQLValueString($_POST['username'], "text"),
-                       GetSQLValueString($_POST['password'], "text"),
-                       GetSQLValueString($_POST['clientRole'], "text"));
+                       GetSQLValueString($_POST['emergencyContactName'], "text"),
+                       GetSQLValueString($_POST['emgncyContactAdd'], "text"),
+                       GetSQLValueString($_POST['emergencyContactNumber'], "int"),
+                       GetSQLValueString($_POST['finAgreement'], "text"));
 
   mysql_select_db($database_MAMP, $MAMP);
   $Result1 = mysql_query($insertSQL, $MAMP) or die(mysql_error());
 
-  $insertGoTo = "adminAddClient.php";
+  $insertGoTo = "adminClientRegSuccess.php";
   if (isset($_SERVER['QUERY_STRING'])) {
     $insertGoTo .= (strpos($insertGoTo, '?')) ? "&" : "?";
     $insertGoTo .= $_SERVER['QUERY_STRING'];
@@ -149,26 +152,23 @@ if ((isset($_POST["MM_insert"])) && ($_POST["MM_insert"] == "addClientForm")) {
   header(sprintf("Location: %s", $insertGoTo));
 }
 
-$colname_user = "-1";
+$colname_users = "-1";
 if (isset($_SESSION['MM_Username'])) {
-  $colname_user = $_SESSION['MM_Username'];
+  $colname_users = $_SESSION['MM_Username'];
 }
 mysql_select_db($database_MAMP, $MAMP);
-$query_user = sprintf("SELECT * FROM Users WHERE userName = %s", GetSQLValueString($colname_user, "text"));
-$user = mysql_query($query_user, $MAMP) or die(mysql_error());
-$row_user = mysql_fetch_assoc($user);
-$totalRows_user = mysql_num_rows($user);
+$query_users = sprintf("SELECT * FROM Users WHERE userName = %s", GetSQLValueString($colname_users, "text"));
+$users = mysql_query($query_users, $MAMP) or die(mysql_error());
+$row_users = mysql_fetch_assoc($users);
+$totalRows_users = mysql_num_rows($users);
 
-$colname_addClient = "-1";
-if (isset($_SESSION['MM_Username'])) {
-  $colname_addClient = $_SESSION['MM_Username'];
-}
 mysql_select_db($database_MAMP, $MAMP);
-$query_addClient = sprintf("SELECT * FROM Clients WHERE Username = %s", GetSQLValueString($colname_addClient, "text"));
-$addClient = mysql_query($query_addClient, $MAMP) or die(mysql_error());
-$row_addClient = mysql_fetch_assoc($addClient);
-$totalRows_addClient = mysql_num_rows($addClient);
-?>
+$query_registerClient = "SELECT * FROM Clients";
+$registerClient = mysql_query($query_registerClient, $MAMP) or die(mysql_error());
+$row_registerClient = mysql_fetch_assoc($registerClient);
+$totalRows_registerClient = mysql_num_rows($registerClient);
+ 
+@session_start(); ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
@@ -183,7 +183,7 @@ h1 {
 <link href="SpryAssets/SpryValidationPassword.css" rel="stylesheet" type="text/css" />
 <link href="SpryAssets/SpryValidationTextField.css" rel="stylesheet" type="text/css" />
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<title>RegisterClient</title>
+<title>Register Client</title>
 <script src="SpryAssets/SpryValidationConfirm.js" type="text/javascript"></script>
 <script src="SpryAssets/SpryValidationPassword.js" type="text/javascript"></script>
 <script src="SpryAssets/SpryValidationTextField.js" type="text/javascript"></script>
@@ -191,7 +191,13 @@ h1 {
 
 <body>
 <div id="holder">
-<div id="header"></div>
+<div id="header">
+  <table width="200" border="0" align="right">
+    <tr>
+      <td>Role: <?php echo $row_users['role']; ?></td>
+    </tr>
+  </table>
+</div>
 <div id="navBar">
 	<nav>
     	<ul>
@@ -207,13 +213,14 @@ h1 {
   </div>
 <div id="content">
 	<div id="pageHeading">
-	  <h1>Clients</h1>
+	  <h1>Register New Client</h1>
 	</div>
     <div id="contentLeft">
       <h3>Links      </h3>
-      <p><a href="adminAddClient.php">Register Client</a>      </p>
-      <p><a href="#">Update Client</a></p>
-      <p><a href="#">Delete Client</a></p>
+      <p><a href="adminClientList.php">Client List</a></p>
+      <p><a href="adminRegisterClient.php">Register Client</a>      </p>
+      <p><a href="adminUpdateClient.php">Update Client</a></p>
+      <p><a href="adminDeleteClient.php">Delete Client</a></p>
       <p>&nbsp;</p>
       <p>&nbsp;</p>
     </div>
@@ -226,122 +233,133 @@ h1 {
         <div id="contentRight2">
           <table width="600" height="429" border="0" align="center">
             <tr>
-              <td width="213">Role:</td>
-              <td width="377"><label for="clientRole"></label>
+              <td width="213" height="30">Role:</td>
+              <td width="377" height="30"><label for="clientRole"></label>
                 <select name="clientRole" id="clientRole">
                   <option value="Client">Client</option>
               </select></td>
             </tr>
             <tr>
-              <td>First Name:</td>
-              <td><span id="sprytextfield1">
+              <td height="30">First Name:</td>
+              <td height="30"><span id="sprytextfield1">
                 <label for="firstName"></label>
                 <input type="text" name="firstName" id="firstName" />
                 <span class="textfieldRequiredMsg">A value is required.</span></span></td>
 </tr>
             <tr>
-              <td>Last Name:</td>
-              <td><span id="sprytextfield2">
+              <td height="30">Last Name:</td>
+              <td height="30"><span id="sprytextfield2">
                 <label for="lastName"></label>
                 <input type="text" name="lastName" id="lastName" />
                 <span class="textfieldRequiredMsg">A value is required.</span></span></td>
 </tr>
             <tr>
-              <td>NHI Number:</td>
-              <td><span id="sprytextfield3">
+              <td height="30">NHI Number:</td>
+              <td height="30"><span id="sprytextfield3">
                 <label for="nhiNumber"></label>
                 <input type="text" name="nhiNumber" id="nhiNumber" />
                 <span class="textfieldRequiredMsg">A value is required.</span></span></td>
 </tr>
             <tr>
-              <td>Gender:</td>
-              <td><label for="genderOption"></label>
+              <td height="30">Gender:</td>
+              <td height="30"><label for="genderOption"></label>
                 <select name="genderOption" id="genderOption">
                   <option value="Male">Male</option>
                   <option value="Female">Female</option>
               </select></td>
             </tr>
             <tr>
-              <td>Contact Number:</td>
-              <td><span id="sprytextfield4">
+              <td height="30">Contact Number:</td>
+              <td height="30"><span id="sprytextfield4">
                 <label for="contactNumber"></label>
                 <input type="text" name="contactNumber" id="contactNumber" />
                 <span class="textfieldRequiredMsg">A value is required.</span></span></td>
 </tr>
             <tr>
-              <td>ServiceID:</td>
-              <td><p><br />
-                  <label for="services"></label>
-                  <select name="services" size="1" id="services">
-                    <option value="1111">Red House</option>
-                    <option value="1234" selected="selected">Blue House</option>
-                    <option value="0987">Orange House</option>
-                  </select>
-                  <br />
-                  <br />
-              </p></td>
-</tr>
+              <td height="30">Assign Support Worker:</td>
+              <td height="30"><label for="assignSW"></label>
+                <select name="assignSW" id="assignSW">
+                  <option value="Support1">Support1</option>
+                  <option value="Support2">Support2</option>
+              </select></td>
+            </tr>
             <tr>
-              <td>Email:</td>
-              <td><span id="sprytextfield6">
+              <td height="30">ServiceID:</td>
+              <td height="30"><select name="serviceMenu" size="1" id="serviceMenu">
+                <option value="1111">1111</option>
+                <option value="1234">1234</option>
+                <option value="0987">0987</option>
+              </select></td>
+            </tr>
+            <tr>
+              <td height="30">service Area:</td>
+              <td height="30"><label for="areaMenu"></label>
+                <select name="areaMenu" size="1" id="areaMenu">
+                  <option value="Area1">Area1</option>
+                  <option value="Area2">Area2</option>
+                  <option value="Area3">Area3</option>
+              </select></td>
+            </tr>
+            <tr>
+              <td height="30">Email:</td>
+              <td height="30"><span id="sprytextfield6">
               <label for="email"></label>
               <input type="text" name="email" id="email" />
               <span class="textfieldRequiredMsg">A value is required.</span><span class="textfieldInvalidFormatMsg">Invalid format.</span></span></td>
 </tr>
             <tr>
-              <td>Emergency Contact Name:</td>
-              <td><span id="sprytextfield8">
+              <td height="30">Emergency Contact Name:</td>
+              <td height="30"><span id="sprytextfield8">
                 <label for="emergencyContactName"></label>
                 <input type="text" name="emergencyContactName" id="emergencyContactName" />
               <span class="textfieldRequiredMsg">A value is required.</span></span></td>
             </tr>
             <tr>
-              <td>Emergency Contact Number:</td>
-              <td><span id="sprytextfield9">
+              <td height="30">Emergency Contact Number:</td>
+              <td height="30"><span id="sprytextfield9">
                 <label for="emergencyContactNumber"></label>
                 <input type="text" name="emergencyContactNumber" id="emergencyContactNumber" />
               <span class="textfieldRequiredMsg">A value is required.</span></span></td>
             </tr>
             <tr>
-              <td>Emergency Contact Address:</td>
-              <td><span id="sprytextfield11">
+              <td height="30">Emergency Contact Address:</td>
+              <td height="30"><span id="sprytextfield11">
                 <label for="emgncyContactAdd"></label>
                 <input type="text" name="emgncyContactAdd" id="emgncyContactAdd" />
               <span class="textfieldRequiredMsg">A value is required.</span></span></td>
             </tr>
             <tr>
-              <td>Financial Agreement:</td>
-              <td><label for="finAgreement"></label>
+              <td height="30">Financial Agreement:</td>
+              <td height="30"><label for="finAgreement"></label>
               <input type="file" name="finAgreement" id="finAgreement" /></td>
             </tr>
             <tr>
-              <td>Username:</td>
-              <td><span id="sprytextfield7">
+              <td height="30">Username:</td>
+              <td height="30"><span id="sprytextfield7">
                 <label for="username"></label>
                 <input type="text" name="username" id="username" />
                 <span class="textfieldRequiredMsg">A value is required.</span></span></td>
 </tr>
             <tr>
-              <td>Password:</td>
-              <td><span id="sprypassword1">
+              <td height="30">Password:</td>
+              <td height="30"><span id="sprypassword1">
                 <label for="password"></label>
                 <input type="password" name="password" id="password" />
                 <span class="passwordRequiredMsg">A value is required.</span></span></td>
 </tr>
             <tr>
-              <td>Confirm Password:</td>
-              <td><span id="spryconfirm1">
+              <td height="30">Confirm Password:</td>
+              <td height="30"><span id="spryconfirm1">
                 <label for="confirmPassword"></label>
                 <input type="password" name="confirmPassword" id="confirmPassword" />
                 <span class="confirmRequiredMsg">A value is required.</span><span class="confirmInvalidMsg">The values don't match.</span></span></td>
 </tr>
             <tr>
-              <td>&nbsp;</td>
-              <td>&nbsp;</td>
+              <td height="30">&nbsp;</td>
+              <td height="30">&nbsp;</td>
             </tr>
             <tr>
-              <td><input type="submit" name="registerButton" id="registerButton" value="Register" /></td>
-              <td>&nbsp;</td>
+              <td height="30" colspan="2" align="center"><input type="submit" name="registerButton" id="registerButton" value="Register" /></td>
             </tr>
           </table>
           <input type="hidden" name="MM_insert" value="registerUserForm" />
@@ -380,9 +398,9 @@ var sprytextfield11 = new Spry.Widget.ValidationTextField("sprytextfield11");
 </body>
 </html>
 <?php
-mysql_free_result($user);
+mysql_free_result($users);
 
-mysql_free_result($addClient);
+mysql_free_result($registerClient);
 
 //file properties
 $file = $_FILES['finAgreement']['tmp_name'];
